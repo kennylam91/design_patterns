f51eb4048d835628a9fcb3f6bd43b8c75a018bcb
kennylam91 <phamvanlam1991@gmail.com>
2022-10-10 09:55:05 +0700

    Analyze the situation of SimUDuck app

--- src/main/java/behavior/strategy/simUDuck/Duck.java
+++ src/main/java/behavior/strategy/simUDuck/Duck.java
@@ -2,11 +2,22 @@ package behavior.strategy.simUDuck;
 
 public abstract class Duck {
 
-    void quack(){
+//    With the inheritance, we have to possibly override fly() and quack() for every new Duck subclass
+
+//    How about take the fly() out of the Duck super class, and make a Flyable interface with a fly() method
+//    That way, only Ducks that are supposed to fly will implement that interface and have a fly() method
+//    That is the dumbest idea, because we have to override the fly() method for all existing Duck subclass
+//    And also the code is basically duplicated
+
+
+//    Design principle
+//    Identify the aspects of your application that vary and separate them from what stays the same
+
+    void quack() {
         System.out.println("Quack");
     }
 
-    void swim(){
+    void swim() {
         System.out.println("Swim");
     }
 
--- src/main/java/behavior/strategy/simUDuck/RubberDuck.java
+++ src/main/java/behavior/strategy/simUDuck/RubberDuck.java
@@ -1,8 +1,14 @@
 package behavior.strategy.simUDuck;
 
 public class RubberDuck extends Duck {
+
     @Override
     void display() {
         System.out.println("look like Rubber duck");
+    }
+
+    @Override
+    void quack() {
+        System.out.println("Squeak");
     }
 }
